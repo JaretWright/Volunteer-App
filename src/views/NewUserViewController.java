9640cf8a858085ffbcfc6e4db4564381514ae683
jaret_000 <jaret_000@192.168.0.30>
2017-10-26 07:39:35 -0400

    Added code for the video "Volunteer App - new Volunteer with custom image".  Video # 7 in the YouTube channel

--- src/views/NewUserViewController.java
+++ src/views/NewUserViewController.java
@@ -11,11 +11,14 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import models.Volunteer;
 
@@ -34,32 +37,75 @@ public class NewUserViewController implements Initializable {
     @FXML private ImageView imageView;
     
     private File imageFile;
+    private boolean imageFileChanged;
+    
     
     /**
-     * This method will read from the scene and try to create a new instance of a Volunteer.
-     * If a volunteer was successfully created, it is updated in the database.
+     * When this button is pushed, a FileChooser object is launched to allow the user
+     * to browse for a new image file.  When that is complete, it will update the 
+     * view with a new image
      */
-    public void saveVolunteerButtonPushed(ActionEvent event)
+    public void chooseImageButtonPushed(ActionEvent event)
     {
-        try
+        //get the Stage to open a new window (or Stage in JavaFX)
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        //Instantiate a FileChooser object
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image");
+        
+        //filter for .jpg and .png
+        FileChooser.ExtensionFilter jpgFilter = new FileChooser.ExtensionFilter("Image File (*.jpg)", "*.jpg");
+        FileChooser.ExtensionFilter pngFilter = new FileChooser.ExtensionFilter("Image File (*.png)", "*.png");
+        fileChooser.getExtensionFilters().addAll(jpgFilter, pngFilter);
+        
+        //Set to the user's picture directory or user directory if not available
+        String userDirectoryString = System.getProperty("user.home")+"\\Pictures";
+        File userDirectory = new File(userDirectoryString);
+        
+        //if you cannot navigate to the pictures directory, go to the user home
+        if (!userDirectory.canRead())
+            userDirectory = new File(System.getProperty("user.home"));
+        
+        fileChooser.setInitialDirectory(userDirectory);
+        
+        //open the file dialog window
+        File tmpImageFile = fileChooser.showOpenDialog(stage);
+        
+        if (tmpImageFile != null)
         {
-            Volunteer volunteer = new Volunteer(firstNameTextField.getText(),lastNameTextField.getText(),
-                                                phoneTextField.getText(), birthday.getValue());
-            errMsgLabel.setText("");    //do not show errors if creating Volunteer was successful
-            volunteer.insertIntoDB();
+            imageFile = tmpImageFile;
+        
+            //update the ImageView with the new image
+            if (imageFile.isFile())
+            {
+                try
+                {
+                    BufferedImage bufferedImage = ImageIO.read(imageFile);
+                    Image img = SwingFXUtils.toFXImage(bufferedImage, null);
+                    imageView.setImage(img);
+                    imageFileChanged = true;
+                }
+                catch (IOException e)
+                {
+                    System.err.println(e.getMessage());
+                }
+            }
         }
-        catch (Exception e)
-        {
-            errMsgLabel.setText(e.getMessage());
-        }
+        
     }
+    
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        errMsgLabel.setText("");
+        birthday.setValue(LocalDate.now().minusYears(18));
+        
+        imageFileChanged = false; //initially the image has not changed, use the default
+        
+        errMsgLabel.setText("");    //set the error message to be empty to start
         
         //load the defautl image for the avatar
         try{
@@ -74,5 +120,35 @@ public class NewUserViewController implements Initializable {
             System.err.println(e.getMessage());
         }
     }    
+    
+    
+    /**
+     * This method will read from the scene and try to create a new instance of a Volunteer.
+     * If a volunteer was successfully created, it is updated in the database.
+     */
+    public void saveVolunteerButtonPushed(ActionEvent event)
+    {
+        try
+        {
+            Volunteer volunteer;
+            if (imageFileChanged)
+            {
+                volunteer = new Volunteer(firstNameTextField.getText(),lastNameTextField.getText(),
+                                                phoneTextField.getText(), birthday.getValue(), imageFile);
+            }
+            else
+            {
+                volunteer = new Volunteer(firstNameTextField.getText(),lastNameTextField.getText(),
+                                                phoneTextField.getText(), birthday.getValue());
+            }
+            errMsgLabel.setText("");    //do not show errors if creating Volunteer was successful
+            volunteer.insertIntoDB();
+        }
+        catch (Exception e)
+        {
+            errMsgLabel.setText(e.getMessage());
+        }
+    }
+    
     
 }
